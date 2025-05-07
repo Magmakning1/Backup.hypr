c1cda236993fe872c5fa947b8903f7b50a2064e5
Magmakning1 <magmakning123456@gmail.com>
2025-05-07 18:18:53 +0200

    Add files via upload

--- Startup.sh
+++ Startup.sh
@@ -1,7 +1,6 @@
 xrandr --output DP-1 --primary
-openrgb -p /home/admin/.config/OpenRGB/ddddd.orp
-hyprctl dispatch exec "[workspace 1 silent] discord"
-hyprctl dispatch exec "[workspace 1 silent] firefox"
+nordvpn c
+openrgb -p /home/admin/.config/OpenRGB/Damn.orp
 
 kill -9 $PPID
 
